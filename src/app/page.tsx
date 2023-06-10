b120cfeee268af13c3aef55214f80dc1d52fa741
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-06-10 02:03:54 +0200

    Speaker componente added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import AiTalker from "@/components/AiTalker";
+import Speaker from "@/components/Speaker";
 
 export default function Home() {
-    return <AiTalker token={process.env.OPENAI_API_KEY!} />;
+    // return <AiTalker token={process.env.OPENAI_API_KEY!} />;
+    return <Speaker />;
 }
---
+++ src/components/Speaker/Speaker.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+function Speaker() {
+    return <p>Hello World!</p>;
+}
+
+export default Speaker;
