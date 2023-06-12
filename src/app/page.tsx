e4e6f2c6b0ebea798a5383fb11c9651d182dea82
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-06-12 10:09:30 +0200

    restart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,25 @@
-import AiTalker from "@/components/AiTalker";
+// import AiTalker from "@/components/AiTalker";
 // import AiSpeaker from "@/components/AiSpeaker";
-// import AiTranscribe from "@/components/AiTranscribe";
+import AiTranscribe from "@/components/AiTranscribe";
 
 export default function Home() {
-    return (
-        <AiTalker
-            token={process.env.OPENAI_API_KEY!}
-            accessKey={process.env.AWS_ACCESS_KEY!}
-            secretKey={process.env.AWS_SECRET_KEY!}
-        />
-    );
+    // return (
+    //     <AiTalker
+    //         token={process.env.OPENAI_API_KEY!}
+    //         accessKey={process.env.AWS_ACCESS_KEY!}
+    //         secretKey={process.env.AWS_SECRET_KEY!}
+    //     />
+    // );
     // return (
     //     <AiSpeaker
     //         accessKey={process.env.AWS_ACCESS_KEY!}
     //         secretKey={process.env.AWS_SECRET_KEY!}
     //     />
     // );
-    // return (
-    //     <AiTranscribe
-    //         accessKey={process.env.AWS_ACCESS_KEY!}
-    //         secretKey={process.env.AWS_SECRET_KEY!}
-    //     />
-    // );
+    return (
+        <AiTranscribe
+            accessKey={process.env.AWS_ACCESS_KEY!}
+            secretKey={process.env.AWS_SECRET_KEY!}
+        />
+    );
 }
