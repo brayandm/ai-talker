aa75a9667ca5713d680a1ce709a2464484dc24d0
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-06-28 12:45:41 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,22 @@ import AiSpeaker from "@/components/AiSpeaker";
 import AiTranscribe from "@/components/AiTranscribe";
 
 export default function Home() {
-    return (
-        <AiTalker
-            accessKey={process.env.AWS_ACCESS_KEY!}
-            secretKey={process.env.AWS_SECRET_KEY!}
-        />
-    );
+    // return (
+    //     <AiTalker
+    //         accessKey={process.env.AWS_ACCESS_KEY!}
+    //         secretKey={process.env.AWS_SECRET_KEY!}
+    //     />
+    // );
     // return (
     //     <AiSpeaker
     //         accessKey={process.env.AWS_ACCESS_KEY!}
     //         secretKey={process.env.AWS_SECRET_KEY!}
     //     />
     // );
-    // return (
-    //     <AiTranscribe
-    //         accessKey={process.env.AWS_ACCESS_KEY!}
-    //         secretKey={process.env.AWS_SECRET_KEY!}
-    //     />
-    // );
+    return (
+        <AiTranscribe
+            accessKey={process.env.AWS_ACCESS_KEY!}
+            secretKey={process.env.AWS_SECRET_KEY!}
+        />
+    );
 }
